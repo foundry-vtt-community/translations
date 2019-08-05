8af1e9714e5d947a3cebe16e9189c0c144fdfa41
Anthony Vadala <git@anthonyvadala.me>
2019-08-05 13:52:08 -0400

    Inital commit

---
+++ .admin/header.md
@@ -0,0 +1,19 @@
+# Foundry VTT Translations Repository
+
+Foundry translations that work across all or most systems are noted here.
+
+To clone this repository, along with every translation in it, use the following command:
+
+```
+git clone --recurse-submodules https://github.com/foundry-vtt-community/translations.git
+```
+
+Instead, if you'd like to clone this repository and only fetch a specific translation from it, use the following commands:
+
+```
+git clone https://github.com/foundry-vtt-community/translations.git
+cd translations
+git submodule init
+git submodule update name-of-the-translation
+```
+
---
+++ .admin/update_translations.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# From https://stackoverflow.com/questions/296536/how-to-urlencode-data-for-curl-command
+
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER)
+  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
+
+# Clear translations file
+echo > translations.md
+topdir=$(pwd)
+
+# Remove any csplit files from a previously aborted job
+rm -f csplit-sections-*
+
+# Split the community translations by sections
+csplit ../wiki.wiki/Community-translations.md '/^# /' '{*}' --prefix csplit-sections- > /dev/null
+
+for section in csplit-sections-* ; do
+	cd $topdir
+	# Get first line and Trim leading/trailing whitespace
+	section_name=$(head -n 1 $section | cut -d' ' -f 2- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+	if [[ "$section_name" != "" && "$section_name" != "Appendix" ]]; then
+		echo "Found section : $section_name"
+		# Split the section into modules
+		csplit $section '/^## /' '{*}' --prefix csplit-modules- > /dev/null
+		for module in csplit-modules-* ; do
+			name=$(head -n 1 $module | cut -d' ' -f 2- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+			if [[ "$name" == "$section_name" ]] ; then
+				cat $module >> translations.md
+				continue
+			fi
+			if [[ "$name" == "(Translation Name)" ]] ; then continue; fi
+			url=$(grep 'https://git' $module | cut -d[ -f 2 | cut -d] -f 1)
+			# Get first line of the description
+			description=$(cat $module | grep -A 1 '### Description' | tail -n 1)
+			echo "Found translation : $name"
+			if [[ "$description" != "" || "$url" != "" ]] ; then
+				echo "   Description : $description"
+				mkdir -p "$section_name"
+				dir=$name
+				if [[ "$url" != "" ]] ; then
+					echo "   URL : $url"
+					dir=$(basename $url)
+					if [[ -d "$section_name/$dir" ]] ; then
+						git submodule update --remote "$section_name/$dir"
+					else
+						git submodule add $url "$section_name/$dir"
+					fi
+				fi
+				mv $module "${section_name}/${dir}.md"
+				git add "${section_name}/${dir}.md"
+				link="$(rawurlencode "${section_name}")/$(rawurlencode "${dir}").md"
+				echo "## [$name]($link)" >> translations.md
+				echo $description >> translations.md
+				echo "" >> translations.md
+			else
+				echo "Skipping this one"
+			fi
+			echo ""
+		done
+		rm -f csplit-modules-*
+	fi
+done
+rm -f csplit-sections-*
+
+cat header.md translations.md > translations.md
+rm translations.md
